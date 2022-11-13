aef226207121c1abfa02799082a8159d72a350b2
eikbo <eikb@itu.dk>
2022-11-13 16:18:08 +0100

    d

--- haha.cs
+++ haha.cs
@@ -35,6 +35,14 @@ Console.writeLine("owo");
 Console.writeLine("uwu");
 Console.writeLine("owo");
 Console.writeLine("owo");
+Console.writeLine("uwu");nsole.writeLine("owo");
+Console.writeLine("uwu");
+Console.writeLine("owo");
+
+Console.writeLine("uwu");
+Console.writeLine("owo");
+Console.writeLine("uwu");
+Console.writeLine("owo");
 Console.writeLine("uwu");
 Console.writeLine("owo");
 
